dcccdf04386b38a9a055d6a71c2021f4e32a2931
Outerthought - Marc Portier <mpo@apache.org>
2002-08-22 16:07:50 +0000

    [wip] refactoring build - status-update Still not functional, but parsing already. Changed my dirty tabs from initial checkin. Started to fill in some body. PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/xml/forrest/trunk@7398 13f79535-47bb-0310-9956-ffa450edef68

--- src/resources/forrest-shbat/bin/forrest.bat
+++ src/resources/forrest-shbat/bin/forrest.bat
@@ -5,13 +5,14 @@ Rem ----- Test if ant is around ----------------------------------------
 if %ANT_HOME%a==a goto noant
 
 Rem ----- set the current working dir as the PROJECT_HOME variable  ----
-set PROJECT_HOME=.
+call %FORREST_HOME%\bin\setpwdvar.bat 
+set PROJECT_HOME=%PWD%
 
 Rem ----- set the ant file to use --------------------------------------
 set ANTFILE=%FORREST_HOME%\forrest.build.xml
 
 Rem ----- call ant.. ---------------------------------------------------
-call %ANT_HOME%\bin\ant -buildfile %ANTFILE% -Dproject.home=%PROJECT_HOME% -emacs -logger org.apache.tools.ant.NoBannerLogger %1 %2 %3 %4 %5 %6 %7 %8 %9
+call %ANT_HOME%\bin\ant -buildfile %ANTFILE% -Dproject.home=%PROJECT_HOME% -Dforrest.home=%FORREST_HOME% -emacs -logger org.apache.tools.ant.NoBannerLogger %1 %2 %3 %4 %5 %6 %7 %8 %9
 
 goto end
 
--- src/resources/forrest-shbat/bin/forrest.sh
+++ src/resources/forrest-shbat/bin/forrest.sh
@@ -5,8 +5,8 @@
 # and bail out if it does not with a message that it is required
 
 if [  "$ANT_HOME" = "" ] ; then
-  echo You must install Ant (http://jakarta.apache.org/ant) 
-	echo and set ANT_HOME to point at your Ant Installation directory
+  echo "You must install Ant (http://jakarta.apache.org/ant)"
+  echo "and set ANT_HOME to point at your Ant Installation directory."
   exit 1
 fi
 
@@ -15,7 +15,7 @@ fi
 PROJECT_HOME=$PWD
 
 # set the ant file to use
-ANTFILE=$FORREST_HOME\forrest.build.xml
+ANTFILE=$FORREST_HOME/forrest.build.xml
 
 # call ant.
-$ANT_HOME/bin/ant -buildfile $ANTFILE -Dproject.home=$PROJECT_HOME -emacs -logger org.apache.tools.ant.NoBannerLogger $@ 
+$ANT_HOME/bin/ant -buildfile $ANTFILE -Dproject.home=$PROJECT_HOME -Dforrest.home=$FORREST_HOME -emacs -logger org.apache.tools.ant.NoBannerLogger $@ 
