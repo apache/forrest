06d6cc83af752f9abe5cb016c5468df76e87fc63
David Crossley <crossley@apache.org>
2002-08-25 03:04:26 +0000

    Tweaks to the new shbat functionality: - Users need not have ANT_HOME set, just 'ant' in their PATH - more robust. - The $FORREST_HOME is set properly (!= PROJECT_HOME). - Automatically fix permissions on forrest.sh Submitted by: Jeff Turner <jefft@apache.org>
    git-svn-id: https://svn.apache.org/repos/asf/xml/forrest/trunk@7403 13f79535-47bb-0310-9956-ffa450edef68

--- src/resources/forrest-shbat/bin/forrest.sh
+++ src/resources/forrest-shbat/bin/forrest.sh
@@ -4,18 +4,20 @@
 # ----- Test if ant is around ------------------------------------------------
 # and bail out if it does not with a message that it is required
 
-if [  "$ANT_HOME" = "" ] ; then
+if [ ! `which ant` ]; then
   echo "You must install Ant (http://jakarta.apache.org/ant)"
-  echo "and set ANT_HOME to point at your Ant Installation directory."
+  echo "and add \$ANT_HOME/bin to your PATH variable"
   exit 1
 fi
 
 
 # set the current working dir as the PROJECT_HOME variable 
 PROJECT_HOME=$PWD
+# use the location of this script to infer $FORREST_HOME
+FORREST_HOME=`dirname $0`/..
 
 # set the ant file to use
 ANTFILE=$FORREST_HOME/forrest.build.xml
 
 # call ant.
-$ANT_HOME/bin/ant -buildfile $ANTFILE -Dproject.home=$PROJECT_HOME -Dforrest.home=$FORREST_HOME -emacs -logger org.apache.tools.ant.NoBannerLogger $@ 
+ant -buildfile $ANTFILE -Dproject.home=$PROJECT_HOME -Dforrest.home=$FORREST_HOME -emacs $@ 
