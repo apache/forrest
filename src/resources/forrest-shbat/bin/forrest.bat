81c0ce256bef3301efcf8173340f148de1ae44ba
Jeff Turner <jefft@apache.org>
2002-10-30 08:55:14 +0000

    Use the embedded Ant, so users don't have to install it
    git-svn-id: https://svn.apache.org/repos/asf/xml/forrest/trunk@7505 13f79535-47bb-0310-9956-ffa450edef68

--- src/resources/forrest-shbat/bin/forrest.bat
+++ src/resources/forrest-shbat/bin/forrest.bat
@@ -1,12 +1,9 @@
 @echo off
 setlocal 
 
-Rem ----- Test if ant is around ----------------------------------------
-if "%ANT_HOME%a"=="a" goto noant
-
 Rem ----- set the current working dir as the PROJECT_HOME variable  ----
 call "%FORREST_HOME%\bin\setpwdvar.bat"
-set PROJECT_HOME=%PWD%
+set PROJECT_HOME="%PWD%"
 
 Rem ----- use the location of this script to infer $FORREST_HOME -------
 set FORREST_HOME=%~dp0\..
@@ -14,18 +11,18 @@ set FORREST_HOME=%~dp0\..
 Rem ----- set the ant file to use --------------------------------------
 set ANTFILE="%FORREST_HOME%\forrest.build.xml"
 
+Rem ----- Save old ANT_HOME --------------------------------------------
+set OLD_ANT_HOME=%ANT_HOME%
+set ANT_HOME="%FORREST_HOME%\ant"
+
 echo "Apache Forrest.  Run 'forrest -projecthelp' to list options"
 echo
 
 Rem ----- call ant.. ---------------------------------------------------
 call "%ANT_HOME%\bin\ant" -buildfile %ANTFILE% -Dbasedir="%PROJECT_HOME%" -Dproject.home="%PROJECT_HOME%" -Dforrest.home="%FORREST_HOME%" -emacs -logger org.apache.tools.ant.NoBannerLogger %1 %2 %3 %4 %5 %6 %7 %8 %9
 
-goto end
+Rem ---- Restore old ANT_HOME
+set ANT_HOME=%OLD_ANT_HOME%
+set CLASSPATH=%OLD_CLASSPATH%
 
-:noant
-echo You must install Ant (http://jakarta.apache.org/ant) 
-echo and set ANT_HOME to point at your Ant Installation directory
-goto end
-
-:end
 endlocal
