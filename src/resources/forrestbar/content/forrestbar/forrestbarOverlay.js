4328aebf86490139a5d8c550a0be9a8165a60165
Nicola Ken Barozzi <nicolaken@apache.org>
2002-11-16 15:29:15 +0000

    <action dev="NKB" type="update" context="docs">         Added enhancements to status.xml to comply with the needs for a complete status file,         like other "STATUS" text files at Apache.         Stylesheets not yet done.       </action>       <action dev="NKB" type="update" context="forrestbar">         Added buttons for nightly Gump runs and local forrest. Corrected and completed         existing interface. We need icons for the new buttons.       </action>
    git-svn-id: https://svn.apache.org/repos/asf/xml/forrest/trunk@7698 13f79535-47bb-0310-9956-ffa450edef68

--- src/resources/forrestbar/content/forrestbar/forrestbarOverlay.js
+++ src/resources/forrestbar/content/forrestbar/forrestbarOverlay.js
@@ -29,23 +29,30 @@ function navProject(searchID) {
 }
 
 function navNews() {
-	navigate('http://xml.apache.org/news.html');
+	navigate('http://www.apache.org/foundation/news.html');
 }
 
 function navDownload() {
-	navigate('http://xml.apache.org/dist/');
+	navigate('http://www.apache.org/dyn/closer.cgi');
 }
 
+function navGumpNightly() {
+	navigate('http://cvs.apache.org/builds/gump/latest/');
+}
+	
 function navCode() {
-	navigate('http://xml.apache.org/cvs.html');
+	navigate('http://cvs.apache.org/viewcvs/');
 }
 
 function navTools() {
 	navigate('http://nagoya.apache.org/');
 }
 
+function navLocalForrest() {
+	navigate('http://127.0.0.1:8888/');
+}
+
 function navAskSam() {
 	navigate('mailto:forrest-user@xml.apache.org');
 }
-
 
