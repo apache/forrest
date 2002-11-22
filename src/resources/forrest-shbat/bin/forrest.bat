9b82a0da6b709cdc6bc2df8ee8e006e2e16d9d88
Jeff Turner <jefft@apache.org>
2002-11-22 10:33:21 +0000

    Backport a bugfix:
    <action dev="JT" type="fix" context="shbat" due-to="Omar Belhaj"
       due-to-email="belhaj@online.no">
     Fixed a bug in forrest.bat, which would fail if run from a different
     drive to that Forrest was installed in.
    </action>
    
    
    git-svn-id: https://svn.apache.org/repos/asf/xml/forrest/branches/FORREST_02_BRANCH@7750 13f79535-47bb-0310-9956-ffa450edef68

--- src/resources/forrest-shbat/bin/forrest.bat
+++ src/resources/forrest-shbat/bin/forrest.bat
@@ -19,7 +19,10 @@ set ANT_HOME=%FORREST_HOME%\ant
 rem ----- Save and set CLASSPATH --------------------------------------------
 set OLD_CLASSPATH=%CLASSPATH%
 set CLASSPATH=
-cd "%FORREST_HOME%\lib\endorsed"
+rem Change drive and directory to %FORREST_HOME%\lib\endorsed
+if "%OS%"=="Windows_NT" cd /d ""%FORREST_HOME%\lib\endorsed""
+if not "%OS%"=="Windows_NT" cd ""%FORREST_HOME%\lib\endorsed""
+
 for %%i in (*.jar) do call "%FORREST_HOME%\bin\appendcp.bat" "%FORREST_HOME%\lib\endorsed\%%i"
 
 echo.
