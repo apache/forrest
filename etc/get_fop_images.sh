fc0114b3d5747622be86180ce1f8881bf4350a19
Jeff Turner <jefft@apache.org>
2003-05-25 05:32:58 +0000

    Remove obsolete PDF info
    git-svn-id: https://svn.apache.org/repos/asf/xml/forrest/trunk@8397 13f79535-47bb-0310-9956-ffa450edef68

--- etc/get_fop_images.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-echo "Fetching latest FOP and patched Cocoon jars..."
-cd ../lib/core && cvs up -r with_pdf_images fop-20030320.jar cocoon-20030311.jar \
- cocoon-fop-block-20030311.jar
-echo "... done"
-echo "Now you must:"
-echo " - download JIMI from http://java.sun.com/products/jimi/ and add jimi.jar to the lib/core directory."
-echo " - delete lib/core/fop-0.20.4.jar"
