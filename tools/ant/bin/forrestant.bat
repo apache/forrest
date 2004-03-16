fbc7dfd95781be139817dac9d08695a1094584ac
David Crossley <crossley@apache.org>
2004-03-16 05:36:52 +0000

    Replace with Apache License 2.0
    git-svn-id: https://svn.apache.org/repos/asf/xml/forrest/trunk@9516 13f79535-47bb-0310-9956-ffa450edef68

--- tools/ant/bin/forrestant.bat
+++ tools/ant/bin/forrestant.bat
@@ -1,7 +1,18 @@
-@echo off
+rem Copyright 2002-2004 The Apache Software Foundation
+rem
+rem Licensed under the Apache License, Version 2.0 (the "License");
+rem you may not use this file except in compliance with the License.
+rem You may obtain a copy of the License at
+rem
+rem     http://www.apache.org/licenses/LICENSE-2.0
+rem
+rem Unless required by applicable law or agreed to in writing, software
+rem distributed under the License is distributed on an "AS IS" BASIS,
+rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+rem See the License for the specific language governing permissions and
+rem limitations under the License.
 
-REM   Copyright (c) 2001-2002 The Apache Software Foundation.  All rights
-REM   reserved.
+@echo off
 
 if exist "%HOME%\antrc_pre.bat" call "%HOME%\antrc_pre.bat"
 
