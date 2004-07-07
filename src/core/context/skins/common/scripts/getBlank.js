54b7ff6aac16ed73b5a928b270804cad42c09339
Thorsten Scherler <thorsten@apache.org>
2004-07-07 12:24:26 +0000

    added ASF licence
    git-svn-id: https://svn.apache.org/repos/asf/forrest/trunk@22672 13f79535-47bb-0310-9956-ffa450edef68

--- src/core/context/skins/common/scripts/getBlank.js
+++ src/core/context/skins/common/scripts/getBlank.js
@@ -1,3 +1,26 @@
+/*
+* Copyright 2002-2004 The Apache Software Foundation
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+/**
+ * This script, when included in a html file, will set the value of a form text to ""
+ * if the text value is the standard value. Besides that is has to be called from the text field
+ *
+ * Typical usage:
+ * <script type="text/javascript" language="JavaScript" src="getBlank.js"></script>
+ * <input type="text" id="query" value="Search the site:" onFocus="getBlank (this, 'Search the site:');"/>
+ */
 <!--
 function getBlank (form, stdValue){
 if (form.value == stdValue){
