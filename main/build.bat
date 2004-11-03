9dd3962fab4529b5490a536b9b9ecdbc98866b35
Juan Jose Pablos <cheche@apache.org>
2004-11-03 09:49:56 +0000

    The lib dir is now up one level.
    git-svn-id: https://svn.apache.org/repos/asf/forrest/trunk@56481 13f79535-47bb-0310-9956-ffa450edef68

--- main/build.bat
+++ main/build.bat
@@ -18,7 +18,7 @@ set ANT_HOME=..\tools\ant
 
 set OLD_CLASSPATH=%CLASSPATH%
 set CLASSPATH=
-for %%i in (.\lib\endorsed\*.jar) do call appendcp.bat %%i
+for %%i in (..\lib\endorsed\*.jar) do call appendcp.bat %%i
 
 echo Using classpath: "%CLASSPATH%"
 call %ANT_HOME%\bin\ant -Djava.endorsed.dirs=lib\endorsed -logger org.apache.tools.ant.NoBannerLogger -emacs %1 %2 %3 %4 %5 %6 %7 %8 %9
