8265ea868820d67c8f69eb8a44b312d77e551dd8
Ross Duncan Gardler <rgardler@apache.org>
2004-12-13 17:41:41 +0000

    some refactoring for more readable code
    git-svn-id: https://svn.apache.org/repos/asf/forrest/trunk@111726 13f79535-47bb-0310-9956-ffa450edef68

--- tools/eclipse/src/org/apache/forrest/ForrestRunner.java
+++ tools/eclipse/src/org/apache/forrest/ForrestRunner.java
@@ -44,16 +44,26 @@ public class ForrestRunner extends Job  {
 	 */
 	private static final Logger logger = Logger.getLogger(ForrestRunner.class);
 
+
+	public static final String COMMAND_BUILD = "site";
+	public static final String COMMAND_START = "run";
 	public static final String COMMAND_STOP = "stop";
+	
+	private static final int UNKOWN_COMMAND = 0;
+	private static final int BUILD = 1;
+	private static final int START = 2;
+	private static final int STOP = 3;
 	
 	public static final int EXCEPTION_UNIDENTIFIED = 1001;
 	public static final int EXCEPTION_UNABLE_TO_STOP = 1010;
+	public static final int EXCEPTION_UNKOWN_COMMAND = 1020;
 	
 	private int stop_port = Integer.getInteger("STOP.PORT",8079).intValue();
 	private String _key = System.getProperty("STOP.KEY","mortbay");
 	
 	private static ForrestRunner INSTANCE;
-	protected String cmdString;
+	private int commandCode;
+	private String workingDir;
 	
 	/**
 	 * Create a Forrest runner to execute a specified command on a given directory
@@ -69,18 +79,15 @@ public class ForrestRunner extends Job  {
 		DOMConfigurator.configure(strLog4jConf);
 		
 		if (cmd.equals(COMMAND_STOP)) {
-			cmdString=COMMAND_STOP;
+			commandCode = STOP;
+		} else if (cmd.equals(COMMAND_START)){
+			commandCode = START;
+		} else if (cmd.equals(COMMAND_BUILD)) {
+			commandCode = BUILD;
 		} else {
-			String fhome = ForrestPlugin.getDefault().getPluginPreferences()
-			  .getString(ForrestPreferences.FORREST_HOME);
-			StringBuffer sb = new StringBuffer("-Dproject.home=");
-			sb.append(workingDir);
-			sb.append(" -Dbasedir=");
-			sb.append(fhome + File.separatorChar + "main");
-			sb.append(" ");
-			sb.append(cmd);
-			cmdString = sb.toString();
+			commandCode = UNKOWN_COMMAND;
 		}
+		this.workingDir = workingDir;
 		INSTANCE = this;
 	}
 	
@@ -91,26 +98,15 @@ public class ForrestRunner extends Job  {
 		if (logger.isDebugEnabled()) {
 			logger.debug("run(IProgressMonitor) - start");
 		}
-		
 		IStatus status;
+		String cmdString = null;
 		
-		if (cmdString.equals("COMMAND_STOP")) {
-			status = this.stop();
+		if (commandCode == STOP) {
+	    	status = this.stop();
+		} else if (commandCode != UNKOWN_COMMAND){
+		    status = runAnt(monitor, getAntCommand());
 		} else {
-			String fhome = ForrestPlugin.getDefault().getPluginPreferences()
-			  .getString(ForrestPreferences.FORREST_HOME);
-			AntRunner runner = new AntRunner();
-			String antFile = fhome + File.separatorChar + "main" + File.separatorChar + "forrest.build.xml";
-			try {
-				runner.setBuildFileLocation(antFile);
-				runner.setArguments(cmdString);
-				logger.info("run() - Running ANT with command string = " + cmdString);
-				runner.run(monitor);
-				status = Status.OK_STATUS;
-			} catch (CoreException e) {
-				logger.error("run(IProgressMonitor)", e);
-				status = new Status(Status.ERROR, null, ForrestRunner.EXCEPTION_UNIDENTIFIED, "Forrest Exception", null);
-			}
+			status = new Status(Status.ERROR, null, ForrestRunner.EXCEPTION_UNKOWN_COMMAND, "Unkown Forrest Command", null);
 		}
 
 		if (logger.isDebugEnabled()) {
@@ -120,17 +116,77 @@ public class ForrestRunner extends Job  {
 	}
 	
 	/**
-	 * Start this version of Forrest.
-	 *
+	 * @return a string that is passed to ANT as a command
 	 */
-	public void start(){
+	private String getAntCommand() {
 		if (logger.isDebugEnabled()) {
-			logger.debug("start() - start");
+			logger.debug("getAntCommand(int) - start");
+		}
+
+		String cmdString;
+		String fhome = ForrestPlugin.getDefault().getPluginPreferences()
+		  .getString(ForrestPreferences.FORREST_HOME);
+		StringBuffer sb = new StringBuffer("-Dproject.home=");
+		sb.append(workingDir);
+		sb.append(" -Dbasedir=");
+		sb.append(fhome + File.separatorChar + "main");
+		sb.append(" ");
+		
+		switch (commandCode) {
+			case START:
+				sb.append(COMMAND_START);
+				break;
+		    case STOP:
+		    	sb = null;
+				break;
+		    case BUILD:
+		    	sb.append(COMMAND_BUILD);
+		}
+		
+		if (sb != null) {
+			cmdString = sb.toString();
+		} else {
+			cmdString = null;
+		}
+		
+		if (logger.isDebugEnabled()) {
+			logger.debug("getAntCommand(int) - end");
+		}
+		return cmdString;
+	}
+
+	/**
+	 * @param monitor
+	 * @param cmdString
+	 * @return
+	 */
+	private IStatus runAnt(IProgressMonitor monitor, String cmdString) {
+		if (logger.isDebugEnabled()) {
+			logger.debug("runAnt(IProgressMonitor, String) - start");
+		}
+
+		IStatus status = Status.OK_STATUS;
+		
+		if (cmdString != null) {
+			String fhome = ForrestPlugin.getDefault().getPluginPreferences()
+			  .getString(ForrestPreferences.FORREST_HOME);
+			AntRunner runner = new AntRunner();
+			String antFile = fhome + File.separatorChar + "main" + File.separatorChar + "forrest.build.xml";
+			try {
+				runner.setBuildFileLocation(antFile);
+				runner.setArguments(cmdString);
+				logger.info("Running ANT with command string = " + cmdString);
+				runner.run(monitor);
+			} catch (CoreException e) {
+				logger.error("run(IProgressMonitor)", e);
+				status = new Status(Status.ERROR, null, ForrestRunner.EXCEPTION_UNIDENTIFIED, "Forrest Exception", null);
+			}
 		}
 
 		if (logger.isDebugEnabled()) {
-			logger.debug("start() - end");
+			logger.debug("runAnt(IProgressMonitor, String) - end");
 		}
+		return status;
 	}
 	
 	/**
