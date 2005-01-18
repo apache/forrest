ffe6641a5c742500a07181314dc34a4f86cb5a2e
Ross Duncan Gardler <rgardler@apache.org>
2005-01-18 23:03:05 +0000

    improve logging messages
    git-svn-id: https://svn.apache.org/repos/asf/forrest/trunk@125560 13f79535-47bb-0310-9956-ffa450edef68

--- tools/eclipse/src/org/apache/forrest/eclipse/job/AntBuildListener.java
+++ tools/eclipse/src/org/apache/forrest/eclipse/job/AntBuildListener.java
@@ -1,9 +1,15 @@
 package org.apache.forrest.eclipse.job;
 
-import org.apache.log4j.Logger;
+import java.net.URL;
 
+import org.apache.forrest.eclipse.ForrestPlugin;
+import org.apache.log4j.Logger;
+import org.apache.log4j.xml.DOMConfigurator;
 import org.apache.tools.ant.BuildEvent;
 import org.apache.tools.ant.BuildListener;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Platform;
+import org.osgi.framework.Bundle;
 
 /**
  * A listener for Ant Builds, all events are logged to the standard logger.
@@ -20,55 +26,59 @@ public class AntBuildListener implements BuildListener{
 	 */
 	public AntBuildListener() {
 		super();
-		// TODO Auto-generated constructor stub
+		Bundle bundle = Platform.getBundle(ForrestPlugin.ID);
+		URL log4jConf = Platform.find(bundle, new Path("conf/log4j.xml"));
+		DOMConfigurator.configure(log4jConf);
 	}
 
 	/* (non-Javadoc)
 	 * @see org.apache.tools.ant.BuildListener#buildFinished(org.apache.tools.ant.BuildEvent)
 	 */
 	public void buildFinished(BuildEvent event) {
-		logger.debug("Ant Finished Build: " + event.getMessage());
+		logger.info("Ant Finished Build: " + event.getMessage());
 	}
 	
 	/* (non-Javadoc)
 	 * @see org.apache.tools.ant.BuildListener#buildStarted(org.apache.tools.ant.BuildEvent)
 	 */
 	public void buildStarted(BuildEvent event) {
-		logger.debug("Ant Started Build: " + event.getMessage());
+		logger.info("Ant Started Build: " + event.getMessage());
 	}
 	
 	/* (non-Javadoc)
 	 * @see org.apache.tools.ant.BuildListener#messageLogged(org.apache.tools.ant.BuildEvent)
 	 */
 	public void messageLogged(BuildEvent event) {
-		logger.debug("Anr Message: " + event.getMessage());
+		logger.info("Ant Message: " + event.getMessage());
 	}
 	
 	/* (non-Javadoc)
 	 * @see org.apache.tools.ant.BuildListener#targetFinished(org.apache.tools.ant.BuildEvent)
 	 */
 	public void targetFinished(BuildEvent event) {
-		logger.debug("Ant Target Finished: " + event.getMessage());
+		logger.info("Ant Target Finished: " + 
+				event.getTarget().getName());
 	}
 	/* (non-Javadoc)
 	 * @see org.apache.tools.ant.BuildListener#targetStarted(org.apache.tools.ant.BuildEvent)
 	 */
 	public void targetStarted(BuildEvent event) {
-		logger.debug("Ant Target Started: " + event.getMessage());
+		logger.info("Ant Target Started: " + 
+				event.getTarget().getName());
 	}
 	
 	/* (non-Javadoc)
 	 * @see org.apache.tools.ant.BuildListener#taskFinished(org.apache.tools.ant.BuildEvent)
 	 */
 	public void taskFinished(BuildEvent event) {
-		logger.debug("Ant Task Finished: " + event.getMessage());
+		logger.debug("Ant Task Finished: " + event.getTask().getTaskName());
 	}
 	
 	/* (non-Javadoc)
 	 * @see org.apache.tools.ant.BuildListener#taskStarted(org.apache.tools.ant.BuildEvent)
 	 */
 	public void taskStarted(BuildEvent event) {
-		logger.debug("Ant Task Started: " + event.getMessage());
+		logger.debug("Ant Task Started: " + event.getTask().getTaskName());
 
 	}
 }
