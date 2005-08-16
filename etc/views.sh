0a6aed5318a3245e62a7a548d8c49c9640b6c89a
Thorsten Scherler <thorsten@apache.org>
2005-08-16 00:28:30 +0000

    Now the script can be run from everywhere. Added CYGWin support.
    git-svn-id: https://svn.apache.org/repos/asf/forrest/trunk@232918 13f79535-47bb-0310-9956-ffa450edef68

--- etc/views.sh
+++ etc/views.sh
@@ -1,6 +1,61 @@
 #!/bin/sh
-echo "\$PWD: $PWD needs to be equal to \$FORREST_HOME: $FORREST_HOME"
-cd $PWD/whiteboard/plugins/org.apache.forrest.plugin.internal.view/
+# Copyright 2002-2004 The Apache Software Foundation or its licensors,
+# as applicable.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+# set the current working dir as the PROJECT_HOME variable 
+cygwin=false;
+darwin=false;
+case `uname` in
+  CYGWIN*) cygwin=true ;;
+  Darwin*) darwin=true
+           if [ -z "$JAVA_HOME" ] ; then
+             JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Home   
+           fi
+           ;;
+esac
+
+if [ "$cygwin" = "true" ] ; then
+  PROJECT_HOME=`cygpath -w "$PWD"`
+else
+  PROJECT_HOME=`pwd`
+fi
+
+if [ -z "$FORREST_HOME" ] ; then
+  # use the location of this script to infer $FORREST_HOME
+  whoami=`basename $0`
+  whereami=`echo $0 | sed -e "s#^[^/]#\`pwd\`/&#"`
+  whereami=`dirname $whereami`
+
+  # Resolve any symlinks of the now absolute path, $whereami
+  realpath_listing=`ls -l $whereami/$whoami`
+  case "$realpath_listing" in
+    *-\>\ /*)
+      realpath=`echo $realpath_listing | sed -e "s#^.*-> ##"`
+      ;;
+    *-\>*)
+      realpath=`echo $realpath_listing | sed -e "s#^.*-> #$whereami/#"`
+      ;;
+    *)
+      realpath=$whereami/$whoami
+      ;;
+  esac
+  FORREST_HOME=`dirname "$realpath"`/..
+fi
+
+cd $FORREST_HOME/whiteboard/plugins/org.apache.forrest.plugin.internal.view/
 ant local-deploy
 cd ../org.apache.forrest.plugin.output.viewHelper.xhtml/
 ant local-deploy
