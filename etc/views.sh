36fc575478e5258600571412b7f17812b5a10a5c
Thorsten Scherler <thorsten@apache.org>
2005-08-16 00:17:47 +0000

    fixed path for the views.sh. This script needs to be run in /home/thorsten/apache/forrest-trunk/
    git-svn-id: https://svn.apache.org/repos/asf/forrest/trunk@232915 13f79535-47bb-0310-9956-ffa450edef68

--- etc/views.sh
+++ etc/views.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-
-cd ../whiteboard/plugins/org.apache.forrest.plugin.internal.view/
+echo "\$PWD: $PWD needs to be equal to \$FORREST_HOME: $FORREST_HOME"
+cd $PWD/whiteboard/plugins/org.apache.forrest.plugin.internal.view/
 ant local-deploy
 cd ../org.apache.forrest.plugin.output.viewHelper.xhtml/
 ant local-deploy
