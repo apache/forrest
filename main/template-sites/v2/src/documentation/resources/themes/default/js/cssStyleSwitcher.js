8584b87b517c4834ecd2e05cea8d49e5773c9856
Thorsten Scherler <thorsten@apache.org>
2005-10-31 12:37:39 +0000

    Finished the theme switcher. In the end I used the cookie stuff submited by Kevin. Thanks a lot Kevin for your contribution that helped a lot. I enhanced the code by adding an example of a dynamic select box which contains all themes that one can select.
    git-svn-id: https://svn.apache.org/repos/asf/forrest/trunk@329808 13f79535-47bb-0310-9956-ffa450edef68

--- main/template-sites/v2/src/documentation/resources/themes/default/js/cssStyleSwitcher.js
+++ main/template-sites/v2/src/documentation/resources/themes/default/js/cssStyleSwitcher.js
@@ -21,20 +21,22 @@
 * cssStyleSwitcher.js
 */
 function switchTheme(title){
-  for(var i = 0; i <document.getElementsByTagName("link").length; i++) {
-    var a = document.getElementsByTagName("link")[i];
+  var linkElements= document.getElementsByTagName("link");
+  for(var i = 0; i <linkElements.length; i++) {
+    var a = linkElements[i];
      //deactivate all screen css
     if (a.getAttribute("media") == "screen") {
       a.disabled=true;
     }
   }
   //activate the selected theme 
-  for(var i = 0; i <document.getElementsByTagName("link").length; i++) {
-    var a = document.getElementsByTagName("link")[i];
+  for(var i = 0; i <linkElements.length; i++) {
+    var a = linkElements[i];
     if (a.getAttribute("media") == "screen" ) {
       a.disabled = (a.getAttribute("title") == title)?false:true;
     }
   }
+  createCookie("style", title, 365);
 }
 /* change the active (preferred) stylesheet to the selected one and save it */
 function switchThemeSelect(selBox){
@@ -43,3 +45,59 @@ function switchThemeSelect(selBox){
    if (title == "-1") return false;
      switchTheme(title); // do the actual switch
 } // end method switchThemeSelect()
+function initSelectSwitcher(themeSwitcherSelect){
+  var select = $(themeSwitcherSelect);
+  var themes=aviableThemes();
+  var tempTheme = themes.split(';');
+  for(var xi=0;xi < tempTheme.length;xi++) {
+    if (tempTheme[xi].length>1){
+      select.options[select.options.length] = new Option(tempTheme[xi]);
+    }
+  }
+}
+function aviableThemes(){
+  var currentTheme;
+  var themes="";
+  var linkElements= document.getElementsByTagName("link");
+  for(var i = 0; i <linkElements.length; i++) {
+    var a = linkElements[i];
+    if (a.getAttribute("media") == "screen" && a.getAttribute("title")) {
+      currentTheme=a.getAttribute("title");
+      var tempTheme = themes.split(';');
+      var contained = false;
+      for(var xi=0;xi < tempTheme.length;xi++) {
+        var theme = tempTheme[xi];
+        if (theme==currentTheme){
+          contained=true;
+        }
+      }
+      if (contained==false){
+        themes=themes+currentTheme+";";
+      }
+    }
+  }
+  return themes;
+}
+
+/*CookieStuff*/
+function createCookie(name, value, days) {
+	var date = new Date();
+	date.setTime(date.getTime() + (days*24*60*60*1000));
+	var expires = "; expires="+date.toGMTString();
+	document.cookie = name + "=" + value + expires + "; path=/";
+}
+function readCookie(name) {
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0;i < ca.length;i++) {
+		var c = ca[i];
+		while (c.charAt(0)==' ') c = c.substring(1,c.length);
+		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+	}
+	return null;
+}
+function initBrandingThemeSwitcher(){
+	var cookie = readCookie("style");
+	switchTheme((cookie)?cookie:'default');
+}
+initBrandingThemeSwitcher();
