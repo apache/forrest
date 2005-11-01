0cf6b77240809ff34694be3dc710899db811ac61
Thorsten Scherler <thorsten@apache.org>
2005-11-01 14:16:08 +0000

    - Enabled theme-profiling in the theme switcher. In combination with the branding-theme-profiler this contract provides a quick way to create new themes and switch them. The default theme can have unlimited color derivatives. If you turn on the color profiling this contract will activate the default theme in combination with the profiling styles. - Updated the example fv to enable the full power of the contract. - removed static stuff in contract and made all possible values configurable via the fv. - you can choose between an "a-link" switcher, that will output the possible themes in <a href="">theme</a>$seperator... gramatic, or as select box.
    git-svn-id: https://svn.apache.org/repos/asf/forrest/trunk@330061 13f79535-47bb-0310-9956-ffa450edef68

--- main/template-sites/v2/src/documentation/resources/themes/default/js/cssStyleSwitcher.js
+++ main/template-sites/v2/src/documentation/resources/themes/default/js/cssStyleSwitcher.js
@@ -20,10 +20,12 @@
 *
 * cssStyleSwitcher.js
 */
+var THEME_SWITCHER_PROFILING=false;
+var THEME_SWITCHER_DEFAULT_THEME="";
 function switchTheme(title){
-  var linkElements= document.getElementsByTagName("link");
+  var linkElements= document.getElementsByTagName("link"); // get all linked objects
   for(var i = 0; i <linkElements.length; i++) {
-    var a = linkElements[i];
+    var a = linkElements[i];  // get linked object
      //deactivate all screen css
     if (a.getAttribute("media") == "screen") {
       a.disabled=true;
@@ -31,13 +33,50 @@ function switchTheme(title){
   }
   //activate the selected theme 
   for(var i = 0; i <linkElements.length; i++) {
-    var a = linkElements[i];
+    var a = linkElements[i]; // get all linked objects
     if (a.getAttribute("media") == "screen" ) {
-      a.disabled = (a.getAttribute("title") == title)?false:true;
+/* theme profiling will only change a couple of stylesheets so we need to include
+    the default theme on which the profiling is based on*/
+      if (THEME_SWITCHER_PROFILING){
+        if(title.lastIndexOf(THEME_SWITCHER_DEFAULT_THEME)>-1){
+          var profiled = (a.getAttribute("title") == THEME_SWITCHER_DEFAULT_THEME || a.getAttribute("title") == title)?true:false;
+          a.disabled = (profiled)?false:true;
+        }else{
+          a.disabled = (a.getAttribute("title") == title)?false:true;
+        }
+      }else{
+        a.disabled = (a.getAttribute("title") == title)?false:true;
+      }
+    }
+  }
+  var styleElements= document.getElementsByTagName("style");// get all inline style objects
+  for(var i = 0; i <styleElements.length; i++) {
+    var a = styleElements[i];
+     //deactivate all screen css
+    if (a.getAttribute("media") == "screen") {
+      a.disabled=true;
+    }
+  }
+  //activate the selected theme 
+  for(var i = 0; i <styleElements.length; i++) {
+    var a = styleElements[i]; // get inline style object
+    if (a.getAttribute("media") == "screen" ) {
+/* theme profiling will only change a couple of stylesheets so we need to include
+    the default theme on which the profiling is based on*/
+      if (THEME_SWITCHER_PROFILING){
+        if(title.lastIndexOf(THEME_SWITCHER_DEFAULT_THEME)>-1){
+          var profiled = (a.getAttribute("title") == THEME_SWITCHER_DEFAULT_THEME || a.getAttribute("title") == title)?true:false;
+          a.disabled = (profiled)?false:true;
+        }else{
+          a.disabled = (a.getAttribute("title") == title)?false:true;
+        }
+      }else{
+        a.disabled = (a.getAttribute("title") == title)?false:true;
+      }
     }
   }
   createCookie("style", title, 365);
-}
+} // end method switchTheme(title)
 /* change the active (preferred) stylesheet to the selected one and save it */
 function switchThemeSelect(selBox){
    var selIndex= selBox.selectedIndex; // get the selected index
@@ -55,12 +94,43 @@ function initSelectSwitcher(themeSwitcherSelect){
     }
   }
 }
+function initASwitcher(themeSwitcherA,seperator){
+  var select = $(themeSwitcherA); // get the object where we want to insert the response
+  var output = "";// initialize output object
+  var themes = aviableThemes(); // get all aviable themes
+  var tempTheme = themes.split(';'); // get an array out of the themes
+  for(var xi=0;xi < tempTheme.length;xi++) {
+    if (tempTheme[xi].length>1){
+      output=output+"<a href=\"javascript:switchTheme('"+tempTheme[xi]+"')\">"+tempTheme[xi]+"</a>"+seperator; // add the theme to an a object
+    }
+  }
+  var response = output.substring(0, output.lastIndexOf(seperator)); // clean the output trailing seperator
+  $(select).innerHTML=response; // set the response
+}
 function aviableThemes(){
   var currentTheme;
   var themes="";
   var linkElements= document.getElementsByTagName("link");
   for(var i = 0; i <linkElements.length; i++) {
     var a = linkElements[i];
+    if (a.getAttribute("media") == "screen" && a.getAttribute("title")) {
+      currentTheme=a.getAttribute("title");
+      var tempTheme = themes.split(';');
+      var contained = false;
+      for(var xi=0;xi < tempTheme.length;xi++) {
+        var theme = tempTheme[xi];
+        if (theme==currentTheme){
+          contained=true;
+        }
+      }
+      if (contained==false){
+        themes=themes+currentTheme+";";
+      }
+    }
+  }
+  var styleElements= document.getElementsByTagName("style");
+  for(var i = 0; i <styleElements.length; i++) {
+    var a = styleElements[i];
     if (a.getAttribute("media") == "screen" && a.getAttribute("title")) {
       currentTheme=a.getAttribute("title");
       var tempTheme = themes.split(';');
@@ -96,8 +166,14 @@ function readCookie(name) {
 	}
 	return null;
 }
-function initBrandingThemeSwitcher(){
-	var cookie = readCookie("style");
-	switchTheme((cookie)?cookie:'default');
+function initBrandingThemeSwitcher(defaultTheme,profilerActivated){
+  THEME_SWITCHER_PROFILING=profilerActivated;
+  THEME_SWITCHER_DEFAULT_THEME=defaultTheme;
+  var cookie = readCookie("style");
+  switchTheme((cookie)?cookie:defaultTheme);
 }
-initBrandingThemeSwitcher();
+//FIXME: Extract the following method to a helper class
+function startsWith(st, pref)
+{
+  return( (pref.length > 0) && (st.substring(0, pref.length) == pref) );
+}
