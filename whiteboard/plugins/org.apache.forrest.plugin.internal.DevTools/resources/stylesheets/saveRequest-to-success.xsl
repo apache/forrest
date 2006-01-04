7b13c61d3a5d3b3ce9b746846cc2350b32003945
Ross Duncan Gardler <rgardler@apache.org>
2006-01-04 00:41:34 +0000

    Initial dev tools plugin as discussed on list - publishes a page to the local build directory from within 'forrest run'. We still need a nice interface to work with (I've added to the ForrestBar but am not committing as it clutters things). It also needs more feedback regarding the current validity of the site
    git-svn-id: https://svn.apache.org/repos/asf/forrest/trunk@365784 13f79535-47bb-0310-9956-ffa450edef68

---
+++ whiteboard/plugins/org.apache.forrest.plugin.internal.DevTools/resources/stylesheets/saveRequest-to-success.xsl
@@ -0,0 +1,57 @@
+<?xml version="1.0"?>
+<!--
+  Copyright 2002-2005 The Apache Software Foundation or its licensors,
+  as applicable.
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+-->
+
+<xsl:stylesheet version="1.0" 
+  xmlns:xsl="http://www.w3.org/1999/XSL/Transform" 
+  xmlns:h="http://apache.org/cocoon/request/2.0">
+  
+  <!-- The response from the save action -->
+  <xsl:param name="response"/>
+  
+  <xsl:template match="h:request">
+    <document>
+      <header>
+        <title>Site Build</title>
+      </header>
+      <body>
+        <warning>Your site may not be fully generated.</warning>
+        
+        <p>You have sucesfully saved the file to your local staging site. However,
+        no other versions of this page (i.e. PDF, text etc) have been saved, similarly
+        no other pages have been saved.</p>
+        
+        <note>If you have changed any of the navigation or skin/theme files you must
+        regenerate the whole site using the "forrest site" command.</note>
+        
+        <p><link>
+          <xsl:attribute name="href">/<xsl:value-of select="h:requestParameters/h:parameter[@name='URI']/h:value"/></xsl:attribute>
+          Return to the originating page</link>.</p>
+        
+        <section>
+          <title>Details</title>
+          <table>
+            <tr>
+              <th>Staged URL</th>
+              <td>/<xsl:value-of select="h:requestParameters/h:parameter[@name='URI']/h:value"/></td>
+            </tr>
+          </table>
+        </section>
+      </body>
+    </document>
+  </xsl:template>
+</xsl:stylesheet>
---
+++ whiteboard/plugins/org.apache.forrest.plugin.internal.DevTools/src/java/org/apache/forrest/actions/WriteToLocalBuildAction.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright 1999-2005 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.apache.forrest.actions;
+
+import org.apache.avalon.framework.parameters.Parameters;
+import org.apache.avalon.framework.thread.ThreadSafe;
+import org.apache.cocoon.acting.ServiceableAction;
+import org.apache.cocoon.environment.ObjectModelHelper;
+import org.apache.cocoon.environment.Redirector;
+import org.apache.cocoon.environment.Request;
+import org.apache.cocoon.environment.Session;
+import org.apache.cocoon.environment.SourceResolver;
+
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpStatus;
+import org.apache.commons.httpclient.methods.GetMethod;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * An action that writes a given document to the local build space.
+ * 
+ * Returns:
+ * message - a human readable message indicating if everything was succesful.
+ */
+public class WriteToLocalBuildAction extends ServiceableAction implements ThreadSafe {
+    Request request;
+            
+    public String writeFile() {
+      String stagingDir = "D:\\openSource\\forrest\\whiteboard\\plugins\\org.apache.forrest.plugin.internal.DevTools\\build\\site";
+      String url = request.getParameter("URI");
+      String stagedFile = stagingDir + "/" + url;
+    
+      HttpClient client = new HttpClient();
+      GetMethod get = new GetMethod("http://localhost:8888/" + url);
+      
+      try {
+        int statusCode = client.executeMethod(get);
+        
+        if (statusCode != HttpStatus.SC_OK) {
+          return "Failed to get document: " + get.getStatusLine();
+        }
+        
+        byte[] responseBody = get.getResponseBody();
+        
+        File file = new File(stagedFile);
+        FileWriter out = new FileWriter(file);
+        out.write(new String(responseBody));
+        out.close();
+      } catch (Exception e) {
+        return e.getMessage();
+      } finally {
+        get.releaseConnection();
+      }
+      
+      return "File written";
+    }
+    
+    public Map act (Redirector redirector, SourceResolver resolver, Map objectModel, String src, Parameters par) throws Exception {      
+        Map response = new HashMap();
+        
+        request = ObjectModelHelper.getRequest(objectModel);
+        response.put("message", writeFile());
+
+        return response;
+    }
+}
