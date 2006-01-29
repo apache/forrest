061e2dd02722db7578b62aa5c843baad8a4e7ff9
Thorsten Scherler <thorsten@apache.org>
2006-01-29 23:49:03 +0000

    FOR-798 - removing old java classes and moving the RecursiveDirectoryTraversalAction class into the dispatcher.
    git-svn-id: https://svn.apache.org/repos/asf/forrest/trunk@373399 13f79535-47bb-0310-9956-ffa450edef68

--- whiteboard/plugins/org.apache.forrest.plugin.internal.dispatcher/src/java/org/apache/forrest/dispatcher/acting/RecursiveDirectoryTraversalAction.java
+++ whiteboard/plugins/org.apache.forrest.plugin.internal.dispatcher/src/java/org/apache/forrest/dispatcher/acting/RecursiveDirectoryTraversalAction.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.forrest.structurer.acting;
+package org.apache.forrest.dispatcher.acting;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
