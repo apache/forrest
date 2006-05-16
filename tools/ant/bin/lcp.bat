f5558ae6b3e77e40f8c1819cfd5d61047b281c1d
David Crossley <crossley@apache.org>
2006-05-16 07:20:06 +0000

    Updated to Apache-Ant-1.6.5 release, jsch-0.1.28, apache-jakarta-commons-net-1.4.1
    git-svn-id: https://svn.apache.org/repos/asf/forrest/trunk@406862 13f79535-47bb-0310-9956-ffa450edef68

--- tools/ant/bin/lcp.bat
+++ tools/ant/bin/lcp.bat
@@ -1,6 +1,5 @@
 REM
-REM Copyright 2001-2004 The Apache Software Foundation or its licensors,
-REM as applicable.
+REM Copyright  2001-2004 The Apache Software Foundation
 REM
 REM  Licensed under the Apache License, Version 2.0 (the "License");
 REM  you may not use this file except in compliance with the License.
