821672eb207bd3ba9dd591a24e8e2d632a13cfb1
Ross Duncan Gardler <rgardler@apache.org>
2006-12-01 22:13:00 +0000

    Navigation reader extends ChainedReader and reads the navigation file from the filesystem
    git-svn-id: https://svn.apache.org/repos/asf/forrest/trunk@481427 13f79535-47bb-0310-9956-ffa450edef68

--- whiteboard/forrest2/wicket-demo/src/java/org/apache/forrest/examples/reader/NavigationReader.java
+++ whiteboard/forrest2/wicket-demo/src/java/org/apache/forrest/examples/reader/NavigationReader.java
@@ -1,24 +1,22 @@
 package org.apache.forrest.examples.reader;
 
-import java.net.MalformedURLException;
 import java.net.URI;
 
 import org.apache.forrest.core.IController;
 import org.apache.forrest.core.document.AbstractSourceDocument;
-import org.apache.forrest.core.document.XMLSourceDocument;
 import org.apache.forrest.core.exception.ProcessingException;
 import org.apache.forrest.core.locationMap.Location;
-import org.apache.forrest.core.reader.AbstractReader;
+import org.apache.forrest.core.reader.ChainedReader;
 
-public class NavigationReader extends AbstractReader {
+public class NavigationReader extends ChainedReader {
 
 	public AbstractSourceDocument read(IController controller, URI requestURI,
-			Location location, URI sourceURI) throws MalformedURLException,
-			ProcessingException {
-		String content = "<navigation><item label=\"Item 1\"/><item label=\"Item 2\"/><item label=\"Item 3\"/></navigation>";
-		String type = "org.apache.forrest.example.Navigation";
-		XMLSourceDocument doc = new XMLSourceDocument(requestURI, content, type);
-		return doc;
+			Location location, URI sourceURI) throws ProcessingException {
+		return super.read(controller, requestURI, location, sourceURI);
+	}
+	
+	public String getDocType() {
+		return "org.apache.forrest.example.Navigation";
 	}
 
 }
