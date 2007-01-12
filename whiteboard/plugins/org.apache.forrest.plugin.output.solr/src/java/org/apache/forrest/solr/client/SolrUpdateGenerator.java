f165dc509c6a8b945d1f1c50ab59e1b934cb0fe4
Thorsten Scherler <thorsten@apache.org>
2007-01-12 00:15:09 +0000

    Using new input stream support for the the generator to sli down the class and add cli support. Still there are some brokenlinks that I need to figure out to add full cli support against a running solr server.
    git-svn-id: https://svn.apache.org/repos/asf/forrest/trunk@495450 13f79535-47bb-0310-9956-ffa450edef68

--- whiteboard/plugins/org.apache.forrest.plugin.output.solr/src/java/org/apache/forrest/solr/client/SolrUpdateGenerator.java
+++ whiteboard/plugins/org.apache.forrest.plugin.output.solr/src/java/org/apache/forrest/solr/client/SolrUpdateGenerator.java
@@ -1,27 +1,21 @@
 package org.apache.forrest.solr.client;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.util.Map;
 
 import org.apache.avalon.framework.parameters.Parameters;
 import org.apache.avalon.framework.service.ServiceException;
 import org.apache.cocoon.ProcessingException;
-import org.apache.cocoon.environment.Context;
 import org.apache.cocoon.environment.SourceResolver;
 import org.apache.cocoon.generation.ServiceableGenerator;
+import org.apache.excalibur.source.Source;
 import org.apache.excalibur.xml.sax.SAXParser;
 import org.apache.forrest.http.client.PostFile;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
 public class SolrUpdateGenerator extends ServiceableGenerator {
-    private static final String SRC_ID = "srcId";
-
-    private static final String SRC_URL_BASE = "srcUrlBase";
-
-    private static final String SERVER = "http://localhost:8888/";
-
+    
     private static final String DESTINATION_URL = "destinationUrl";
 
     private static final String SOLR_UPDATE_URL = "http://localhost:8983/solr/update";
@@ -30,29 +24,32 @@ public class SolrUpdateGenerator extends ServiceableGenerator {
 
     private SAXParser parser;
 
+    private String destination;
+
     public void setup(SourceResolver resolver, Map objectModel, String src,
             Parameters par) throws ProcessingException, SAXException, IOException {
         super.setup(resolver, objectModel, src, par);
-            try {
-                init(par);
-            } catch (ServiceException e) {
-                throw  new ProcessingException (e);
-            }
-    }
-
-    private void init(Parameters par) throws MalformedURLException, IOException,
-             SAXException, ServiceException {
-        String destination = par.getParameter(DESTINATION_URL, SOLR_UPDATE_URL);
-        String base = par.getParameter(SRC_URL_BASE, SERVER);
-        String srcId = par.getParameter(SRC_ID, null);
-        String srcUrl = base + srcId;
-        post = new PostFile(destination, srcUrl);
-        // Set up to read the input file
-        parser = (SAXParser) manager.lookup(SAXParser.ROLE);
+        destination = par.getParameter(DESTINATION_URL, SOLR_UPDATE_URL);
     }
 
     public void generate() throws IOException, SAXException, ProcessingException {
-        parser.parse(new InputSource(post.getResponseBodyAsStream()),contentHandler);
+        Source inputSource = null;
+        try {
+            resolver = (SourceResolver) manager.lookup(SourceResolver.ROLE);
+            inputSource = resolver.resolveURI(this.source);
+            if (inputSource.exists()) {
+                post = new PostFile(destination, inputSource.getInputStream());
+                // Set up parser to parse the input file
+                parser = (SAXParser) manager.lookup(SAXParser.ROLE);
+                parser.parse(new InputSource(post.getResponseBodyAsStream()),contentHandler);
+            }else{
+                throw new ProcessingException("source: "+this.source+" does not exits");
+            }
+            
+        } catch (ServiceException e) {
+            throw new ProcessingException(e);
+        }
+        
     }
 
 }
