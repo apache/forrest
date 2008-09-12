6282c66b3716929c5e5e6939137d5abde338c369
thorsten <thorsten@13f79535-47bb-0310-9956-ffa450edef68>
2008-09-12 22:31:37 +0000

    Obsolete import
    git-svn-id: https://svn.apache.org/repos/asf/forrest/trunk@694824 13f79535-47bb-0310-9956-ffa450edef68

--- whiteboard/dispatcher/java/org/apache/forrest/dispatcher/impl/XSLContract.java
+++ whiteboard/dispatcher/java/org/apache/forrest/dispatcher/impl/XSLContract.java
@@ -23,7 +23,6 @@ import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.util.HashMap;
 
-import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.transform.Result;
 import javax.xml.transform.Source;
