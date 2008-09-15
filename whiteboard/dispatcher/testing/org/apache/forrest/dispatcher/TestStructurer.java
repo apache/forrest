5385e5be5dd703fe8e09c836f4b2ff1c65feef22
thorsten <thorsten@13f79535-47bb-0310-9956-ffa450edef68>
2008-09-15 11:16:57 +0000

    Adding test with parsing xml properties. However the method that will record the xml of a property is not yet implmeneted
    git-svn-id: https://svn.apache.org/repos/asf/forrest/trunk@695431 13f79535-47bb-0310-9956-ffa450edef68

--- whiteboard/dispatcher/testing/org/apache/forrest/dispatcher/TestStructurer.java
+++ whiteboard/dispatcher/testing/org/apache/forrest/dispatcher/TestStructurer.java
@@ -9,10 +9,19 @@ import junit.framework.TestCase;
 public class TestStructurer extends TestCase {
   private static final String STRUCTURER_XML = "master.structurer.xml";
   public void testStructurer() throws DispatcherException{
+    XMLStructurer structurer = prepareStructurer();
+    structurer.execute();
+  }
+  public void testStructurerWithXmlProperties() throws DispatcherException{
+    XMLStructurer structurer = prepareStructurer();
+    structurer.setAllowXmlProperties(true);
+    structurer.execute();
+  }
+  private XMLStructurer prepareStructurer() {
     InputStream dataStream=this.getClass().getResourceAsStream(STRUCTURER_XML); 
     String format="html";
     XMLStructurer structurer  = new XMLStructurer(dataStream, format);
     structurer.setContractUriPrefix("/org/apache/forrest/dispatcher/");
-    structurer.execute();
+    return structurer;
   }
 }
