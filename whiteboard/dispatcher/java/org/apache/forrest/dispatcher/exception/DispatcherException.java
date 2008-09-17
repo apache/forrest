55cbb37a036e79eb3ddda3eb31b7878c5d2711ec
thorsten <thorsten@13f79535-47bb-0310-9956-ffa450edef68>
2008-09-17 11:27:36 +0000

    Moving class to a better package path
    git-svn-id: https://svn.apache.org/repos/asf/forrest/trunk@696250 13f79535-47bb-0310-9956-ffa450edef68

--- whiteboard/dispatcher/java/org/apache/forrest/dispatcher/exception/DispatcherException.java
+++ whiteboard/dispatcher/java/org/apache/forrest/dispatcher/exception/DispatcherException.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.forrest.dispatcher;
+package org.apache.forrest.dispatcher.exception;
 
 /**
  * Dispatcher Exception
