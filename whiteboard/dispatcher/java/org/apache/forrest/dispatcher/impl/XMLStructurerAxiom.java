3a9995e48ce03fcd2df1157c2e68b5c5a406fd18
thorsten <thorsten@13f79535-47bb-0310-9956-ffa450edef68>
2008-09-26 11:24:20 +0000

    Adding an AXIOM implementation of the structurer that supports advanced injection of content.
    It actually resolves the corresponding node where one want to inject and not like in the StAX only implementation where we are not able to do so since it 100% based on XMLEvents. 
    
    Needed to extend some AXIOM classes (see WSCOMMONS-389) to create not existing xpath and return a node. In the class AXIOMXPathPatched there is still one fix me to support advanced xpath expression like [@id='example'] which currently is not supported for the generation of new paths.
    
    As soon I fixed this I provide a patch and if this patch makes it into the next release we have to declare the class as deprecated and remove it in the next version of the dispatcher.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/forrest/trunk@699276 13f79535-47bb-0310-9956-ffa450edef68

---
+++ whiteboard/dispatcher/java/org/apache/forrest/dispatcher/impl/XMLStructurerAxiom.java
@@ -0,0 +1,374 @@
+package org.apache.forrest.dispatcher.impl;
+
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+
+import javax.xml.namespace.QName;
+import javax.xml.stream.XMLEventWriter;
+import javax.xml.stream.XMLStreamConstants;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+import javax.xml.stream.events.XMLEvent;
+import javax.xml.stream.util.XMLEventAllocator;
+
+import org.apache.axiom.om.OMAbstractFactory;
+import org.apache.axiom.om.OMAttribute;
+import org.apache.axiom.om.OMDocument;
+import org.apache.axiom.om.OMElement;
+import org.apache.axiom.om.OMFactory;
+import org.apache.axiom.om.OMNamespace;
+import org.apache.axiom.om.OMNode;
+import org.apache.axiom.om.impl.builder.StAXOMBuilder;
+import org.apache.axiom.om.xpath.AXIOMXPath;
+import org.apache.forrest.dispatcher.api.Contract;
+import org.apache.forrest.dispatcher.api.Resolver;
+import org.apache.forrest.dispatcher.api.Structurer;
+import org.apache.forrest.dispatcher.config.DispatcherBean;
+import org.apache.forrest.dispatcher.exception.DispatcherException;
+import org.apache.forrest.dispatcher.factories.ContractFactory;
+import org.apache.forrest.dispatcher.impl.helper.AXIOMXPathPatched;
+import org.apache.forrest.dispatcher.impl.helper.Captions;
+import org.apache.forrest.dispatcher.impl.helper.StAX;
+import org.apache.forrest.dispatcher.impl.helper.StreamHelper;
+import org.apache.forrest.dispatcher.utils.CommonString;
+import org.jaxen.JaxenException;
+import org.xml.sax.InputSource;
+
+public class XMLStructurerAxiom extends StAX implements Structurer {
+
+  private static final String PATH_PREFIX = "/result";
+
+  private final Resolver resolver;
+
+  private final boolean allowXmlProperties;
+
+  private final ContractFactory contractRep;
+
+  private LinkedHashMap<String, LinkedHashSet<OMNode>> resultTree = new LinkedHashMap<String, LinkedHashSet<OMNode>>();
+
+  private OMFactory factory;
+
+  private OMElement root;
+
+  public XMLStructurerAxiom(DispatcherBean config) {
+    this.contractRep = new ContractFactory(config);
+    this.resolver = config.getResolver();
+    this.allowXmlProperties = config.isAllowXmlProperties();
+    this.factory = OMAbstractFactory.getOMFactory();
+  }
+
+  /*
+   * @see
+   * org.apache.forrest.dispatcher.impl.Structurer#execute(java.io.InputStream,
+   * java.lang.String)
+   */
+  public InputStream execute(InputStream structurerStream, String format)
+      throws DispatcherException {
+    BufferedInputStream stream = null;
+    ByteArrayOutputStream out = new ByteArrayOutputStream();
+    /*
+     * Preparing the model by creating a root document/element. Afterward we
+     * need to strip the root element again.
+     */
+    OMDocument doc = factory.createOMDocument();
+    root = factory.createOMElement("result", null);
+    doc.addChild(root);
+    String currentPath = PATH_PREFIX;
+    try {
+      /*
+       * Preparing the processing.
+       */
+      XMLStreamReader reader = getReader(structurerStream);
+      StAXOMBuilder builder = new StAXOMBuilder(reader);
+      OMElement strucuturer = builder.getDocumentElement();
+      Iterator<OMNode> structures = strucuturer.getChildrenWithName(new QName(
+          Captions.NS, Captions.STRUCTURE_ELEMENT));
+      /*
+       * process all structure elements within this structurer file.
+       */
+      while (structures.hasNext()) {
+        String m_type = "", path = "";
+        OMNode node = (OMNode) structures.next();
+        if (isElement(node)) {
+          OMElement structure = (OMElement) node;
+          m_type = structure.getAttributeValue(qIt(Captions.TYPE_ATT));
+          path = structure.getAttributeValue(qIt(Captions.HOOKS_ATT));
+          if (format.equals(m_type)) {
+            log.debug("matched - need to process");
+            // adding the default path
+            if (!"/".equals(String.valueOf(path.charAt(0)))) {
+              path = "/" + path;
+            }
+            currentPath += path;
+            AXIOMXPathPatched xpath = new AXIOMXPathPatched(currentPath);
+            Object pathNode = (OMElement) xpath.selectSingleNode(root, true);
+            processStructure(structure, (OMElement) pathNode);
+          }
+        }
+      }
+      /*
+       * In case it strike you odd that we just use the first node this is
+       * because the output of a structurer needs to be well-formed and there
+       * should only be one child in any circumstance.
+       */
+      root.getFirstElement().serialize(out);
+      // debug hook
+      // next line will get the whole result document
+      //root.serialize(out);
+    } catch (XMLStreamException e) {
+      throw new DispatcherException(e);
+    } catch (JaxenException e) {
+      throw new DispatcherException(e);
+    } catch (IOException e) {
+      throw new DispatcherException(e);
+    } finally {
+      if (null != structurerStream) {
+        try {
+          structurerStream.close();
+        } catch (IOException e) {
+          throw new DispatcherException(e);
+        }
+        ;
+      }
+    }
+    stream = StreamHelper.switchStream(out);
+    log.debug(out.toString());
+    return stream;
+  }
+
+  /**
+   * @param node
+   * @return
+   */
+  private boolean isElement(OMNode node) {
+    return node.getType() == OMNode.ELEMENT_NODE;
+  }
+
+  /**
+   * @param structure
+   * @param pathNode
+   * @throws XMLStreamException
+   * @throws DispatcherException
+   * @throws IOException
+   * @throws JaxenException
+   */
+  private void processStructure(OMElement structure, OMElement pathNode)
+      throws XMLStreamException, DispatcherException, IOException,
+      JaxenException {
+    Iterator<OMNode> strucutrerElements = structure.getChildren();
+    while (strucutrerElements.hasNext()) {
+      OMNode node = (OMNode) strucutrerElements.next();
+      if (isElement(node)) {
+        computeNodes((OMElement) node, pathNode);
+      }
+    }
+
+  }
+
+  /**
+   * This is the main iterating process here we process all contracts and hooks.
+   * 
+   * @param node
+   *          current node
+   * @param pathNode
+   *          the model representing the current path
+   * @throws DispatcherException
+   * @throws XMLStreamException
+   * @throws IOException
+   * @throws JaxenException
+   */
+  private void computeNodes(OMElement component, OMElement pathNode)
+      throws DispatcherException, XMLStreamException, IOException,
+      JaxenException {
+    if (component.getQName().equals(
+        new QName(Captions.NS, Captions.CONTRACT_ELEMENT))) {
+      processContract(component, pathNode);
+    } else if (component.getQName().equals(
+        new QName(Captions.NS, Captions.HOOK_ELEMENT))) {
+      processHook(component, pathNode);
+    }
+  }
+
+  /**
+   * @param component
+   * @param pathNode
+   * @throws DispatcherException
+   * @throws XMLStreamException
+   * @throws IOException
+   * @throws JaxenException
+   */
+  private void processHook(OMElement component, OMElement pathNode)
+      throws DispatcherException, XMLStreamException, IOException,
+      JaxenException {
+    OMElement element = factory.createOMElement(component.getLocalName(),
+        component.getNamespace().getNamespaceURI(), "forrest");
+    Iterator<OMAttribute> attributes = component.getAllAttributes();
+    while (attributes.hasNext()) {
+      OMAttribute attribute = attributes.next();
+      element.addAttribute(attribute);
+
+    }
+    pathNode.addChild(element);
+    Iterator<OMNode> childs = component.getChildren();
+    while (childs.hasNext()) {
+      OMNode node = (OMNode) childs.next();
+      if (isElement(node)) {
+        computeNodes((OMElement) node, element);
+      }
+    }
+  }
+
+  /**
+   * @param component
+   * @throws DispatcherException
+   * @throws XMLStreamException
+   * @throws IOException
+   * @throws JaxenException
+   */
+  private void processContract(OMElement component, OMElement pathNode)
+      throws DispatcherException, XMLStreamException, IOException,
+      JaxenException {
+    String name = "", data = null;
+    name = component.getAttributeValue(qIt(Captions.NAME_ATT));
+    data = component.getAttributeValue(qIt(Captions.DATA_ATT));
+    log.debug("Contract: " + name + "\ndataUri: " + data);
+    InputStream dataStream = null;
+    if (null != data && !data.equals("")) {
+      dataStream = resolver.resolve(data);
+    }
+    Contract contract = contractRep.resolve(name);
+    HashMap<String, ?> param = new HashMap();
+    OMElement properties = component
+        .getFirstChildWithName(qIt(Captions.PROPERTY_ELEMENT));
+    if (properties != null) {
+      processProperty(properties, param);
+    }
+    InputStream resultStream = contract.execute(dataStream, param);
+    StreamHelper.closeStream(dataStream);
+    processContractResult(resultStream, pathNode);
+    StreamHelper.closeStream(resultStream);
+  }
+
+  /**
+   * @param resultStream
+   * @param pathNode
+   * @throws XMLStreamException
+   * @throws JaxenException
+   */
+  private void processContractResult(InputStream resultStream,
+      OMElement pathNode) throws XMLStreamException, JaxenException {
+    XMLStreamReader contractResultReader = getReader(resultStream);
+    StAXOMBuilder builder = new StAXOMBuilder(contractResultReader);
+    OMElement content = builder.getDocumentElement();
+    /*
+     *  For full blown ns support we need to get all ns from the
+     *  result. This will be passed later to the XPath processor.
+     */
+    Iterator<OMNamespace> ns = content.getAllDeclaredNamespaces();
+    HashSet<OMNamespace> spaces = new HashSet<OMNamespace>();
+    while (ns.hasNext()) {
+      OMNamespace space =  ns.next();
+      spaces.add(space);
+    }
+    Iterator<OMElement> parts = content.getChildrenWithName(new QName(
+        Captions.NS_CONTRACTS, Captions.PART_ELEMENT));
+    while (parts.hasNext()) {
+      OMElement part = parts.next();
+      String xpath = part.getAttributeValue(qIt(Captions.RESULT_XPATH));
+      Iterator<OMNode> children = part.getChildren();
+      if (xpath == null) {
+        while (children.hasNext()) {
+          OMNode child = (OMNode) children.next();
+          pathNode.addChild(child);
+        }
+      } else {
+        xpath = PATH_PREFIX + xpath;
+        // we need to feed the xpathSelector with the ns we may have
+        AXIOMXPathPatched xpathSelector = new AXIOMXPathPatched(xpath);
+        
+        for (OMNamespace space : spaces) {
+          xpathSelector.addNamespace(space.getPrefix(), space.getNamespaceURI());
+        }
+        OMElement injectionPoint = (OMElement) xpathSelector.selectSingleNode(
+            root, true);
+        while (children.hasNext()) {
+          OMNode child = (OMNode) children.next();
+          injectionPoint.addChild(child);
+        }
+
+      }
+    }
+  }
+
+  /**
+   * @param name
+   * @return
+   */
+  private QName qIt(String name) {
+    return new QName(name);
+  }
+
+  /**
+   * @param properties
+   * @param param
+   * @throws XMLStreamException
+   */
+  private void processProperty(OMElement properties, HashMap param)
+      throws XMLStreamException {
+    String propertyName = null, propertyValue = null;
+    propertyName = properties.getAttributeValue(qIt(Captions.NAME_ATT));
+    propertyValue = properties.getAttributeValue(qIt(Captions.VALUE_ATT));
+    if (allowXmlProperties) {
+      param.put(propertyName, recordXmlProperies(properties
+          .getXMLStreamReader()));
+    } else if (null != propertyValue && null != propertyName) {
+      param.put(propertyName, propertyValue);
+    }
+  }
+
+  /**
+   * @param reader
+   * @return
+   * @throws XMLStreamException
+   */
+  private InputSource recordXmlProperies(XMLStreamReader reader)
+      throws XMLStreamException {
+    ByteArrayOutputStream out = new ByteArrayOutputStream();
+    XMLEventWriter writerProperty = getWriter(out);
+    XMLEventAllocator allocator = getEventAllocator();
+    XMLEvent currentEvent = allocator.allocate(reader);
+    writerProperty.add(currentEvent);
+    boolean process = true;
+    while (process) {
+      int event = reader.next();
+      currentEvent = allocator.allocate(reader);
+      switch (event) {
+      case XMLStreamConstants.END_ELEMENT:
+        if (reader.getLocalName().equals(Captions.PROPERTY_ELEMENT)) {
+          writerProperty.add(currentEvent);
+          writerProperty.flush();
+          writerProperty.close();
+          process = false;
+        } else {
+          writerProperty.add(currentEvent);
+        }
+        break;
+
+      default:
+        writerProperty.add(currentEvent);
+        break;
+      }
+    }
+    InputSource value = new InputSource(new ByteArrayInputStream(out
+        .toByteArray()));
+    return value;
+  }
+}
---
+++ whiteboard/dispatcher/java/org/apache/forrest/dispatcher/impl/helper/AXIOMXPathPatched.java
@@ -0,0 +1,161 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.forrest.dispatcher.impl.helper;
+
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+
+import org.apache.axiom.om.OMAbstractFactory;
+import org.apache.axiom.om.OMElement;
+import org.apache.axiom.om.OMFactory;
+import org.apache.axiom.om.impl.OMNamespaceImpl;
+import org.apache.axiom.om.xpath.AXIOMXPath;
+import org.jaxen.JaxenException;
+
+/**
+ * Issue: WSCOMMONS-389
+ * 
+ * As soon the above issue is fixed we need to drop this implementation 
+ * since it will be in the AXIOMXPath.
+ * 
+ * The process should be 
+ * <ol>
+ * <li> stripping this class that it is only extending super and mark this class as deprecated
+ * <li> remove it the next release.
+ *  
+ * @version 1.0
+ *
+ */
+public class AXIOMXPathPatched extends AXIOMXPath {
+
+  private static final long serialVersionUID = -4670206430863692541L;
+  private String xpathExpr;
+  private OMFactory factory;
+
+  public AXIOMXPathPatched(String xpathExpr) throws JaxenException {
+    super(xpathExpr);
+    this.xpathExpr = xpathExpr;
+    this.factory = OMAbstractFactory.getOMFactory();
+  }
+
+  public Object selectSingleNode(OMElement root, boolean create)
+      throws JaxenException {
+    Object xpathNode = super.selectSingleNode(root);
+    if (xpathNode == null && create) {
+      int endIndex = xpathExpr.lastIndexOf("/");
+      String xpath = xpathExpr.substring(0, endIndex);
+      String rest = xpathExpr.substring(endIndex + 1);
+      xpathNode = createNode(root, xpath, rest);
+    }
+    return xpathNode;
+  }
+
+  private Object createNode(OMElement root, String path, String rest)
+      throws JaxenException {
+    String m_xpath = path;
+    Object xpathNode = null;
+    while (true) {
+      AXIOMXPath xpath = getNsXPath(m_xpath);
+      xpathNode = xpath.selectSingleNode(root);
+      if (xpathNode == null) {
+        int endIndex = path.lastIndexOf("/");
+        m_xpath = path.substring(0, endIndex);
+        rest = path.substring(endIndex + 1) + "/" + rest;
+      } else if (xpathNode != null) {
+        xpathNode = calculatePath(root, (OMElement) xpathNode, rest);
+        break;
+      }
+
+    }
+    return xpathNode;
+  }
+
+  /**
+   * @param root
+   * @param node
+   * @param rest
+   * @return
+   */
+  private Object calculatePath(OMElement root, OMElement node, String rest) {
+    String[] tokenizer = rest.split("/");
+    LinkedHashSet<OMElement> pathNodes = new LinkedHashSet<OMElement>();
+    for (String pathPart : tokenizer) {
+      if (!pathPart.equals("")) {
+        calculateNsElements(pathNodes, pathPart);
+      }
+    }// now we have all nodes created but with no hierarchy
+    OMElement[] elements = new OMElement[0];
+    elements = pathNodes.toArray(new OMElement[pathNodes.size()]);
+    // create the hierarchy by adding the last node to the
+    // previous one till we reached the first one.
+    for (int j = elements.length - 1; j > 0; j--) {
+      elements[j - 1].addChild(elements[j]);
+    }
+    // Get rid of the overhead and expose just one node
+    OMElement firstNode = elements[0];
+    if (node == null) {
+      root.addChild(firstNode);
+    } else {
+      node.addChild(firstNode);
+    }
+    // get the last not since it is the one we are looking for
+    OMElement lastNode = elements[elements.length - 1];
+    return lastNode;
+  }
+
+  /**
+   * @param pathNodes
+   * @param pathPart
+   */
+  private void calculateNsElements(LinkedHashSet<OMElement> pathNodes,
+      String pathPart) {
+    
+    /*
+     * FIXME: Need to test xpath expression such as [@id='example']
+     * I think their are NOT generated correctly!
+     */
+    OMElement element = null;
+    OMNamespaceImpl localSpace = null;
+    String localName = pathPart;
+    String[] nameSpacedNode = localName.split(":");
+    if (nameSpacedNode.length == 2) {
+      final String prefix = nameSpacedNode[0];
+      final String uri = (String) getNamespaces().get(prefix);
+      localSpace = new OMNamespaceImpl(uri, prefix);
+      localName = nameSpacedNode[1];
+    }
+    element = factory.createOMElement(localName, localSpace);
+    pathNodes.add(element);
+  }
+
+  /**
+   * @param path
+   * @return
+   * @throws JaxenException
+   */
+  private AXIOMXPath getNsXPath(String path) throws JaxenException {
+    AXIOMXPath xpath = new AXIOMXPath(path);
+    Iterator iterator = this.getNamespaces().keySet().iterator();
+    while (iterator.hasNext()) {
+      String key = (String) iterator.next();
+      String value = (String) getNamespaces().get(key);
+      xpath.addNamespace(key, value);
+    }
+    return xpath;
+  }
+
+}
